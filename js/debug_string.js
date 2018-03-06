148646995474072ccf35b27fcfd5d0d8ad4c1524
PyCee <kwp.asd@gmail.com>
2018-03-06 11:36:31 -0600

    added temp_debug_string and debug_string (more permanant)

--- js/debug_string.js
+++ js/debug_string.js
@@ -1,7 +1,21 @@
+var temp_debug_string = "";
+var temp_debug_string_element = document.getElementById("temp_debug_string");
+
 var debug_string = "";
 var debug_string_element = document.getElementById("debug_string");
-function Reset_Debug_String () {
-    debug_string = [];
+debug_string_element.style.left = "1000px";
+function Reset_Temp_Debug_String () {
+    temp_debug_string = [];
+    Update_Debug_String();
+}
+function Add_Temp_Debug_String (string) {
+    if(temp_debug_string.length > 1000){
+        return;
+    }
+    if(temp_debug_string.length > 0){
+        temp_debug_string += "</br>";
+    }
+    temp_debug_string += string;
     Update_Debug_String();
 }
 function Add_Debug_String (string) {
@@ -15,5 +29,6 @@ function Add_Debug_String (string) {
     Update_Debug_String();
 }
 function Update_Debug_String () {
+    temp_debug_string_element.innerHTML = temp_debug_string;
     debug_string_element.innerHTML = debug_string;
 }
