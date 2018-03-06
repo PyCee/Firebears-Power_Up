ee23109526872ae72a808b8d200e30bc008f212d
PyCee <kwp.asd@gmail.com>
2018-03-06 11:43:09 -0600

    added switch cube stacking to infinity

--- js/exploration/switch.js
+++ js/exploration/switch.js
@@ -3,9 +3,15 @@ var Switch_Cube_Relative_Positioning = [
     new Vector(0.60, -0.32),
     new Vector(1.05, -0.32),
     new Vector(0.375, -0.64),
-    new Vector(0.825, -0.64),
-    new Vector(0.60, -0.96)
+    new Vector(0.825, -0.64)
 ];
+function Get_Next_Cube_Offset (cube_index) {
+    if(cube_index <= 4){
+        return Switch_Cube_Relative_Positioning[cube_index];
+    } else {
+        return new Vector(0.60, -0.64 - (0.32 * (cube_index - 4)));
+    }
+}
 
 var switch_id_list = [];
 class Switch_Component extends Actor {
@@ -20,7 +26,7 @@ class Switch_Component extends Actor {
         switch_id_list.push(this.id);
     }
     add_cube (cube) {
-        var cube_relative_position = Switch_Cube_Relative_Positioning[this.cube_count];
+        var cube_relative_position = Get_Next_Cube_Offset(this.cube_count);
         cube.set_absolute_position(this.position.add(cube_relative_position));
         cube.physics_state.freeze();
         cube.set_ghost();
