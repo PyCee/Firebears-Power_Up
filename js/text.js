ca05c81a305674fe01fd87a49791ab0f5fb1f3f1
PyCee <kwp.asd@gmail.com>
2018-03-16 19:32:55 -0500

    modified text position/sizing

--- js/text.js
+++ js/text.js
@@ -1,19 +1,22 @@
 class Text extends Renderable {
-    constructor (position, font_size, text, color) {
-	super(position, new Vector(font_size, font_size), null);
-	this.text = text;
-	this.color = color;
+    constructor (position, font_height, text, color) {
+		super(position, new Vector(0.0, 0.0), null, 99);
+		this.font_height = font_height;
+		this.text = text;
+		this.color = color;
     }
     hide(){}
     show(){}
     set_animation(){}
     update_animation(){}
     display () {
-	ctx.textBaseline = "top";
-	ctx.fillStyle = this.color;
-	ctx.font = this.size.y*canvas.width + "px Lucida Console, Monaco, monospace";
-	var pos = this.position.scale(canvas.width);
-	ctx.fillText(this.text, pos.x, pos.y);
-	//ctx.fillText(this.text, this.position.x, this.position.y);
+		ctx.textBaseline = "hanging";
+		
+		ctx.fillStyle = this.color;
+		// ctx.font = this.font_height*canvas.width + "px Lucida Console, Monaco, monospace";
+		ctx.font = (this.font_height * canvas.width) / curr_scene.inside_width +
+			"px Lucida Console, Monaco, monospace";
+		var pos = this.position.scale(canvas.width / curr_scene.inside_width);
+		ctx.fillText(this.text, pos.x, pos.y);
     }
 }
